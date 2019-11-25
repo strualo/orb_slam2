3fb377a6a934c9c0cb11fe46693320daf1d32482
strualo <1464488476@qq.com>
2019-11-25 10:53:43 +0800

    creat orb_slam2 hub

---
+++ cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/slamer/orb_slam2/src/Converter.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Converter.cc.o"
+  "/home/slamer/orb_slam2/src/Frame.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Frame.cc.o"
+  "/home/slamer/orb_slam2/src/FrameDrawer.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/FrameDrawer.cc.o"
+  "/home/slamer/orb_slam2/src/Initializer.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Initializer.cc.o"
+  "/home/slamer/orb_slam2/src/KeyFrame.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/KeyFrame.cc.o"
+  "/home/slamer/orb_slam2/src/KeyFrameDatabase.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/KeyFrameDatabase.cc.o"
+  "/home/slamer/orb_slam2/src/LocalMapping.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/LocalMapping.cc.o"
+  "/home/slamer/orb_slam2/src/LoopClosing.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/LoopClosing.cc.o"
+  "/home/slamer/orb_slam2/src/Map.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Map.cc.o"
+  "/home/slamer/orb_slam2/src/MapDrawer.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/MapDrawer.cc.o"
+  "/home/slamer/orb_slam2/src/MapPoint.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/MapPoint.cc.o"
+  "/home/slamer/orb_slam2/src/ORBextractor.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/ORBextractor.cc.o"
+  "/home/slamer/orb_slam2/src/ORBmatcher.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/ORBmatcher.cc.o"
+  "/home/slamer/orb_slam2/src/Optimizer.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Optimizer.cc.o"
+  "/home/slamer/orb_slam2/src/PnPsolver.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/PnPsolver.cc.o"
+  "/home/slamer/orb_slam2/src/Sim3Solver.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Sim3Solver.cc.o"
+  "/home/slamer/orb_slam2/src/System.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/System.cc.o"
+  "/home/slamer/orb_slam2/src/Tracking.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Tracking.cc.o"
+  "/home/slamer/orb_slam2/src/Viewer.cc" "/home/slamer/orb_slam2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/src/Viewer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/local/lib/cmake/Pangolin/../../../include"
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
